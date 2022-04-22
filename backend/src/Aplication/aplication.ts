48cd6b3288a3b9d8e2f704155254e77dfaff5ff2
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-22 14:12:41 +0300

    feat(backend): :memo: added openAPI spec

--- backend/src/Aplication/aplication.ts
+++ backend/src/Aplication/aplication.ts
@@ -1,10 +1,29 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RepositoryService } from '../Infrastructure/Persistence/Repository/repository.service';
 import { AppModule } from './DI/app.module';
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  addPrismaHooks(app);
+  await app.listen(3000);
+}
+
+async function addPrismaHooks(app: INestApplication) {
   const prismaService = app.get(RepositoryService);
   await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  addSwagger(app);
+  return app;
+}
+
+function addSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Bingewatch API')
+    .setDescription('Bingewatch API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  return app;
 }
