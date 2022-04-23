48f4f11249dedfa5cf9d8ef359df75d0f1607b98
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-23 16:17:57 +0300

    feat(backend): :sparkles: improved user service and controller

--- backend/src/Aplication/Logic/Services/user.service.ts
+++ backend/src/Aplication/Logic/Services/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { UserEntity } from '../../../Domain/Entities/user.entity';
 import { RepositoryService } from '../../../Infrastructure/Persistence/Repository/repository.service';
 import { EncryptionService } from './encryption.service';
 
@@ -8,5 +9,21 @@ export class UserService {
     private readonly repositoryService: RepositoryService,
     private readonly encryptionService: EncryptionService,
   ) {}
-  async create(data: {}) {}
+  async create(data: { password: string; email: string; name: string }) {
+    const passwordHash = await this.encryptionService.hashString(data.password);
+    const user = await this.repositoryService.user.create({
+      data: {
+        passwordHash,
+        email: data.email,
+        name: data.name,
+      },
+    });
+    return new UserEntity(user);
+  }
+  async findOneByEmail(email: string) {
+    const user = await this.repositoryService.user.findUnique({
+      where: { email },
+    });
+    return new UserEntity(user);
+  }
 }
--- backend/src/Aplication/Presentation/HTTP-REST/user.controller.ts
+++ backend/src/Aplication/Presentation/HTTP-REST/user.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
 import { UserService } from '../../Logic/Services/user.service';
 import { CreateUserDto } from '../DTOs/user.dto';
 
@@ -6,6 +13,13 @@ import { CreateUserDto } from '../DTOs/user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Post()
-  async create(@Body() createUserDto: CreateUserDto) {}
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.findOneByEmail(createUserDto.email);
+    if (user) {
+      throw new BadRequestException('Email is already in use');
+    }
+    return await this.userService.create(createUserDto);
+  }
 }
