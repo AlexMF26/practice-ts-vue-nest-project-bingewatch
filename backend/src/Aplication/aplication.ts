9d89e00178068290fb7c2bc58a782cce3288b438
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-23 16:30:07 +0300

    feat(backend): :sparkles: added global pipes

--- backend/src/Aplication/aplication.ts
+++ backend/src/Aplication/aplication.ts
@@ -1,4 +1,4 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { RepositoryService } from '../Infrastructure/Persistence/Repository/repository.service';
@@ -7,13 +7,14 @@ import { AppModule } from './DI/app.module';
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   addPrismaHooks(app);
+  addSwagger(app);
+  addGlobalPipes(app);
   await app.listen(3000);
 }
 
 async function addPrismaHooks(app: INestApplication) {
   const prismaService = app.get(RepositoryService);
   await prismaService.enableShutdownHooks(app);
-  addSwagger(app);
   return app;
 }
 
@@ -25,5 +26,15 @@ function addSwagger(app: INestApplication) {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  return app;
+}
+
+function addGlobalPipes(app: INestApplication) {
+  addValidationPipe(app);
+  return app;
+}
+
+function addValidationPipe(app: INestApplication) {
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   return app;
 }
