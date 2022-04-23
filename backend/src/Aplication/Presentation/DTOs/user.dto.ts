02f71ea629fa952a68b1e7c7a7c30458b6cfc802
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-23 16:29:37 +0300

    docs(backend): :sparkles: improved api property description

--- backend/src/Aplication/Presentation/DTOs/user.dto.ts
+++ backend/src/Aplication/Presentation/DTOs/user.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The name of the user',
+    type: String,
+  })
   @IsString()
   /* Regex to match the name of a person 
     allows for "'", "-" and "," and ".".
@@ -11,14 +14,21 @@ export class CreateUserDto {
     message: 'Name is not valid',
   })
   readonly name: string;
-  @ApiProperty()
+  @ApiProperty({
+    description: 'The email of the user',
+    type: String,
+  })
   @IsString()
   //Regex to check if the email is valid
   @Matches(/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/, {
     message: 'Invalid email',
   })
   readonly email: string;
-  @ApiProperty()
+  @ApiProperty({
+    description:
+      'The password of the user. Must be at least 8 characters long and contain at least one number, one uppercase letter, lowercase letter and one special character',
+    type: String,
+  })
   @IsString()
   /*
     (?!.*\s)          # Must contain no whitespace characters
@@ -32,7 +42,7 @@ export class CreateUserDto {
     /(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
     {
       message:
-        'Your password is too weak.\nUse at least 8 characters, one uppercase, one lowercase, one number and one special character',
+        'Your password is too weak. Use at least 8 characters, one uppercase, one lowercase, one number and one special character',
     },
   )
   readonly password: string;
