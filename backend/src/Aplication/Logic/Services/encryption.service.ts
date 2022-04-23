7dbfb653163416162bf4b7e966c7ffe1dce174dc
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-23 17:52:15 +0300

    docs(backend): :memo: updated comments

--- backend/src/Aplication/Logic/Services/encryption.service.ts
+++ backend/src/Aplication/Logic/Services/encryption.service.ts
@@ -3,11 +3,27 @@ import { hash, compare } from 'bcrypt';
 
 @Injectable()
 export class EncryptionService {
-  public async hashString(data: string) {
-    //10 rounds to generate the salt
+  /**
+   * @param  {string} data The string to be hashed.
+   * @returns {Promise<string>} A promise to be resolved with the hashed string, the salt, the algorithm used and the cost factor.
+   */
+  public async hashString(data: string): Promise<string> {
+    //Use 2^10 rounds
     return await hash(data, 10);
   }
-  public async validateAgainstHash(data: string, hash: string) {
+  /**
+   * @param  {string} data The string to be checked.
+   * @param  {string} hash The bcrypt return string to compare with.
+   * @returns {Promise<boolean>} A promise to be resolved with true if the string is a match, false otherwise.
+   */
+  public async validateAgainstHash(
+    data: string,
+    hash: string,
+  ): Promise<boolean> {
+    /*
+      Take the salt from the hash(bcrypt stores the salt in the hash)
+      hash the data and compare it to the hash
+    */
     return await compare(data, hash);
   }
 }
