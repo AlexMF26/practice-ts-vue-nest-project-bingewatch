29c0ab1ba1d1f7d03d46b51c50157e3ca5e0126f
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-26 00:51:09 +0300

    fix(backend): :bug: update user patch dto

--- backend/src/Aplication/Presentation/HTTP/REST/DTOs/update-role.dto.ts
+++ backend/src/Aplication/Presentation/HTTP/REST/DTOs/update-role.dto.ts
@@ -40,4 +40,30 @@ export class UpdateUserDto {
   @IsEnum(Role)
   @IsOptional()
   readonly role?: Role;
+
+  @ApiPropertyOptional({
+    description:
+      'The password of the user. Must be at least 8 characters long and contain at least one number, one uppercase letter, lowercase letter and one special character.',
+    example: 'P@ssword123',
+    pattern:
+      '(?!.*s)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})',
+  })
+  /*
+    (?!.*\s)          # Must contain no whitespace characters
+    (?=.*[a-z])       # Must contain at least one lowercase letter
+    (?=.*[A-Z])       # Must contain at least one upercase letter
+    (?=.*[0-9])       # Must contain at least one digit
+    (?=.*[!@#\$%\^&]) # Must contain at least one special character
+    (?=.{8,})         # Must be at least 8 characters long
+  */
+  @Matches(
+    /(?!.*\s)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+    {
+      message:
+        'Your password is too weak. Use at least 8 characters, one uppercase, one lowercase, one number and one special character.',
+    },
+  )
+  @IsString()
+  @IsOptional()
+  readonly password?: string;
 }
