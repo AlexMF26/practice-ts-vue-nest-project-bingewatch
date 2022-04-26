a268b6801784c787c943cd212f78b7d150a4e0c7
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-04-26 17:44:47 +0300

    feat(backend): :sparkles: added users patch

--- backend/src/Aplication/Logic/Services/users.service.ts
+++ backend/src/Aplication/Logic/Services/users.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { UserEntity } from '../../../Domain/Entities/user.entity';
+import { Role, UserEntity } from '../../../Domain/Entities/user.entity';
 import { RepositoryService } from '../../../Infrastructure/Persistence/Repository/repository.service';
 import { EncryptionService } from './encryption.service';
 
@@ -12,15 +12,31 @@ export class UsersService {
 
   logger = new Logger(UsersService.name);
 
-  async create(data: { password: string; email: string; name: string }) {
-    this.logger.log(`Creating user with email "${data.email}".`);
+  async register(data: { password: string; email: string; name: string }) {
+    this.logger.log(`Registering user with email "${data.email}".`);
+    const existentUser = await this.findOneByEmail(data.email);
+    if (existentUser) {
+      this.logger.warn(`User with email "${data.email}" already exists.`);
+      throw new Error('Email is already in use.');
+    }
     const passwordHash = await this.encryptionService.hashString(data.password);
+    const userData = {
+      passwordHash,
+      email: data.email,
+      name: data.name,
+    };
+    const user = await this.create(userData);
+    return user;
+  }
+
+  private async create(data: {
+    passwordHash: string;
+    email: string;
+    name: string;
+  }) {
+    this.logger.log(`Creating user with email "${data.email}".`);
     const user = await this.repositoryService.user.create({
-      data: {
-        passwordHash,
-        email: data.email,
-        name: data.name,
-      },
+      data,
     });
     this.logger.log(
       `User "${user.id}" with email "${user.email}" was created.`,
@@ -39,7 +55,7 @@ export class UsersService {
       );
       return new UserEntity(user);
     }
-    this.logger.warn(`User with email "${email}" was not found.`);
+    this.logger.log(`User with email "${email}" was not found.`);
     return null;
   }
 
@@ -52,7 +68,7 @@ export class UsersService {
       this.logger.log(`User "${user.id}" was found.`);
       return new UserEntity(user);
     }
-    this.logger.warn(`User with id "${id}" was not found.`);
+    this.logger.log(`User with id "${id}" was not found.`);
     return null;
   }
 
@@ -70,5 +86,123 @@ export class UsersService {
     }
     this.logger.warn(`User with id "${id}" was not found.`);
     return null;
+  }
+
+  private async update(id: string, data: Partial<UserEntity>) {
+    this.logger.log(`Updating user with id "${id}".`);
+    const user = await this.repositoryService.user.update({
+      where: { id },
+      data,
+    });
+    return new UserEntity(user);
+  }
+
+  private async isAuthorizedToUpdateUserData(id: string, targetId) {
+    this.logger.log(
+      `Checking if user with id "${id}" is allowed to update user with id "${targetId}".`,
+    );
+    if (id === targetId) {
+      this.logger.log(
+        `User with id "${id}" is allowed to update user with id "${targetId}".`,
+      );
+      return true;
+    }
+    const requester = await this.findOneById(id);
+    if (requester.role !== Role.ADMIN) {
+      this.logger.log(
+        `User "${id}" is not allowed to update user "${targetId}".`,
+      );
+      return false;
+    }
+    return true;
+  }
+
+  async changeUserData(
+    id: string,
+    requesterId: string,
+    data: Partial<{
+      password: string;
+      email: string;
+      name: string;
+      role: Role;
+    }>,
+  ) {
+    const isAuthorized = await this.isAuthorizedToUpdateUserData(
+      requesterId,
+      id,
+    );
+    if (!isAuthorized) {
+      this.logger.warn(
+        `User "${requesterId}" is not authorized to update user "${id}".`,
+      );
+      throw new Error(`You are not allowed to update user "${id}".`);
+    }
+    const updateData: any = {};
+    let user = await this.findOneById(id);
+    if (data.role && user.role !== data.role) {
+      if (data.role) {
+        const requesterRole = await this.getRole(requesterId);
+        if (requesterRole !== Role.ADMIN) {
+          this.logger.warn(
+            `User "${requesterId}" is not authorized to roles".`,
+          );
+          throw new Error(`User "${requesterId}" is not authorized to roles".`);
+        } else {
+          this.logger.log(`User ${id} role will be changed to ${data.role}`);
+          updateData.role = data.role;
+        }
+      }
+    }
+    if (data.password) {
+      const isTheSamePassword =
+        await this.encryptionService.validateAgainstHash(
+          data.password,
+          user.passwordHash,
+        );
+      if (!isTheSamePassword) {
+        const passwordHash = await this.encryptionService.hashString(
+          data.password,
+        );
+        this.logger.log(`User "${id}" password will be changed.`);
+        updateData.passwordHash = passwordHash;
+      }
+    }
+    if (data.email && user.email !== data.email) {
+      const existentUser = await this.findOneByEmail(data.email);
+      if (existentUser) {
+        this.logger.warn(`User with email "${data.email}" already exists.`);
+        throw new Error('Email is already in use.');
+      }
+      this.logger.log(`User "${id}" email will be changed.`);
+      updateData.email = data.email;
+    }
+    if (data.name && user.name !== data.name) {
+      this.logger.log(`User "${id}" name will be changed.`);
+      updateData.name = data.name;
+    }
+    if (Object.keys(updateData).length === 0) {
+      this.logger.log(`User "${id}" data was not changed.`);
+    } else {
+      user = await this.update(id, updateData);
+    }
+    return user;
+  }
+
+  async validateCredentials(email: string, password: string) {
+    this.logger.log(`Validating credentials for user with email "${email}".`);
+    const user = await this.findOneByEmail(email);
+    if (!user) {
+      this.logger.warn(`User with email "${email}" doesn't exist exists.`);
+      return false;
+    }
+    const isPasswordValid = await this.encryptionService.validateAgainstHash(
+      password,
+      user.passwordHash,
+    );
+    if (!isPasswordValid) {
+      this.logger.warn('Password is not valid.');
+      return false;
+    }
+    return user;
   }
 }
--- backend/src/Aplication/Presentation/HTTP/REST/Controllers/users.controller.ts
+++ backend/src/Aplication/Presentation/HTTP/REST/Controllers/users.controller.ts
@@ -17,10 +17,7 @@ import {
   ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
-import {
-  Role,
-  SerializedUserEntity,
-} from '../../../../../Domain/Entities/user.entity';
+import { SerializedUserEntity } from '../../../../../Domain/Entities/user.entity';
 import { UsersService } from '../../../../Logic/Services/users.service';
 import { userId } from '../../Decorators/userId.decorator';
 import { JwtGuard } from '../../Guards/jwt.guard';
@@ -44,20 +41,24 @@ export class UsersController {
     this.logger.log(
       `An HTTP request to create a new user with email "${createUserDto.email}" was received.`,
     );
-    const user = await this.usersService.findOneByEmail(createUserDto.email);
-    if (user) {
-      this.logger.warn(
-        `User with email "${createUserDto.email}" already exists.`,
+    try {
+      const user = await this.usersService.register(createUserDto);
+      this.logger.log(
+        `User "${user.id}" with email "${user.email}" was created.`,
       );
-      throw new BadRequestException(
-        `Email "${createUserDto.email}" is already in use.`,
-      );
+      return user;
+    } catch (error) {
+      if (error.message === 'Email is already in use.') {
+        throw new BadRequestException(
+          `User with email "${createUserDto.email}" already exists.`,
+        );
+      } else {
+        this.logger.error(
+          `The following error occurred while registering user: ${error.message}`,
+        );
+        throw error;
+      }
     }
-    const createdUser = await this.usersService.create(createUserDto);
-    this.logger.log(
-      `User "${createdUser.id}" with email "${createdUser.email}" was created.`,
-    );
-    return createdUser;
   }
 
   @ApiCookieAuth('Authentication')
@@ -76,27 +77,7 @@ export class UsersController {
     this.logger.log(
       `An HTTP request to update user "${targetId}" from user "${requesterId}" was received.`,
     );
-    let user = await this.usersService.findOneById(targetId);
-    if (!user) {
-      this.logger.warn(`User "${targetId}" does not exist.`);
-      throw new BadRequestException(`User "${targetId}" does not exist.`);
-    }
-    const requester = await this.usersService.findOneById(requesterId);
-    if (targetId !== requesterId) {
-      if (requester.role !== Role.ADMIN) {
-        this.logger.warn(
-          `User "${requesterId}" is not allowed to update user "${targetId}".`,
-        );
-        return new UnauthorizedException(
-          `You are not allowed to update user "${targetId}".`,
-        );
-      } else {
-        this.logger.log(
-          `User "${requesterId}" is allowed to update user "${targetId}" because of his admin privilege.`,
-        );
-      }
-    }
-    if (!updateUserDto) {
+    if (updateUserDto && Object.keys(updateUserDto).length === 0) {
       this.logger.warn(
         `No data was provided to update "${targetId}" at the request of "${requesterId}".`,
       );
@@ -104,51 +85,30 @@ export class UsersController {
         `No data was provided to update "${targetId}".`,
       );
     }
-    if (updateUserDto.password) {
-      this.logger.log(
-        `Updating user "${targetId}" password at the request of "${requesterId}".`,
+    try {
+      const user = await this.usersService.changeUserData(
+        targetId,
+        requesterId,
+        updateUserDto,
       );
-      user = await this.updatePassword(targetId, updateUserDto.password);
+      return user;
+    } catch (error) {
+      if (error.message.includes('is not authorized to update user')) {
+        throw new UnauthorizedException(
+          `You are not allowed to update user "${targetId}".`,
+        );
+      } else if (error.message.includes('is not authorized to roles')) {
+        throw new UnauthorizedException(`You are not authorized to roles".`);
+      } else if (error.message === 'Email is already in use.') {
+        throw new BadRequestException(
+          `User with email "${updateUserDto.email}" already exists.`,
+        );
+      } else {
+        this.logger.error(
+          `The following error occurred while updating user: ${error.message}`,
+        );
+        throw error;
+      }
     }
-    if (updateUserDto.email) {
-      this.logger.log(
-        `Updating user "${targetId}" email at the request of "${requesterId}".`,
-      );
-      user = await this.updateEmail(targetId, updateUserDto.email);
-    }
-    if (updateUserDto.name) {
-      this.logger.log(
-        `Updating user "${targetId}" name at the request of "${requesterId}".`,
-      );
-      user = await this.updateName(targetId, updateUserDto.name);
-    }
-
-    if (updateUserDto.role) {
-      this.logger.log(
-        `Updating user "${targetId}" role at the request of "${requesterId}".`,
-      );
-      user = await this.updateRole(targetId, updateUserDto.role);
-    }
-    return user;
-  }
-
-  private async updateRole(id: string, role: Role) {
-    const user = await this.usersService.findOneById(id);
-    return user;
-  }
-
-  private async updatePassword(id: string, password: string) {
-    const user = await this.usersService.findOneById(id);
-    return user;
-  }
-
-  private async updateEmail(id: string, email: string) {
-    const user = await this.usersService.findOneById(id);
-    return user;
-  }
-
-  private async updateName(id: string, name: string) {
-    const user = await this.usersService.findOneById(id);
-    return user;
   }
 }
