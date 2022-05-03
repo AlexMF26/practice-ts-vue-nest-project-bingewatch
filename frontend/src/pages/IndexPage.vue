5feb366f9feccf18a8f0a50e1159292c77e0e25a
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-04 01:25:52 +0300

    fix(frontend): :fire: removed unnecesary stuff

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,49 +1,16 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <q-btn class="col-span-1" color="primary" @click="test()" />
   </q-page>
 </template>
 
-<script lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { defineComponent, ref } from 'vue';
-
-export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
-  setup() {
-    const todos = ref<Todo[]>([
-      {
-        id: 1,
-        content: 'ct1',
-      },
-      {
-        id: 2,
-        content: 'ct2',
-      },
-      {
-        id: 3,
-        content: 'ct3',
-      },
-      {
-        id: 4,
-        content: 'ct4',
-      },
-      {
-        id: 5,
-        content: 'ct5',
-      },
-    ]);
-    const meta = ref<Meta>({
-      totalCount: 1200,
-    });
-    return { todos, meta };
-  },
-});
+<script setup lang="ts">
+import { useAuthStore } from '../stores/auth.store';
+const authStore = useAuthStore();
+const test = () => {
+  authStore.login({
+    email: 'Ion.Popescu@google.com',
+    password: 'P@ssworuud123',
+  });
+};
 </script>
