bcc3bb5a5f5ee4f2c7a0de1445d3f34b54f85538
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-04 23:18:38 +0300

    chore(frontend): :wastebasket: cleanup

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -1,21 +1,5 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <q-btn class="col-span-1" color="primary" @click="test()" />
-    <q-btn class="col-span-1" color="primary" @click="test2()" />
-  </q-page>
+  <q-page class="row items-center justify-evenly"> </q-page>
 </template>
 
-<script setup lang="ts">
-import { useAuthStore } from '../stores/auth.store';
-
-const authStore = useAuthStore();
-const test = () => {
-  authStore.login({
-    email: 'Ion.Popescu@google.com',
-    password: 'P@ssword123',
-  });
-};
-const test2 = () => {
-  authStore.logout();
-};
-</script>
+<script setup lang="ts"></script>
