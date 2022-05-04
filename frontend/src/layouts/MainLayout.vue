d1fcc2aae1c2a41d08ef5ebf1440f0e59a047f1b
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-04 21:45:07 +0300

    refactor(frontend): :art: new layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,22 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR lfr">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="logo.svg" />
+          </q-avatar>
+          Bingewatch
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <q-list dense> </q-list>
+    </q-drawer>
+
     <q-page-container>
       <AlertsComponent />
       <router-view />
@@ -8,5 +25,10 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import AlertsComponent from '../components/AlertsComponent.vue';
+const rightDrawerOpen = ref(false);
+function toggleRightDrawer() {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+}
 </script>
