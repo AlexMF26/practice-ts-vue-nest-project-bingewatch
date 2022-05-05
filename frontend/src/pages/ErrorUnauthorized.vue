e1d8a3889e548729fa56290290d70a208cf59332
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-05 13:50:09 +0300

    feat(frontend): :sparkles: added unauthorized error

---
+++ frontend/src/pages/ErrorUnauthorized.vue
@@ -0,0 +1,21 @@
+<template>
+  <div
+    class="fullscreen bg-secondary text-primary text-center q-pa-md flex flex-center"
+  >
+    <div>
+      <div style="font-size: 30vh">401</div>
+
+      <div class="text-h2" style="opacity: 0.4">You shouldn't be here...</div>
+
+      <q-btn
+        class="q-mt-xl"
+        color="primary"
+        text-color="accent"
+        unelevated
+        to="/"
+        label="Go Home"
+        no-caps
+      />
+    </div>
+  </div>
+</template>
--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -7,16 +7,6 @@ const routes: RouteRecordRaw[] = [
     children: [
       { path: '', component: () => import('src/pages/LandingPage.vue') },
       {
-        path: '/entry/:id',
-        component: () => import('src/pages/EntryPage.vue'),
-        props: (route) => ({ id: route.params.id }),
-      },
-      {
-        path: '/watchlist/:id',
-        component: () => import('src/pages/WatchlistPage.vue'),
-        props: (route) => ({ id: route.params.id }),
-      },
-      {
         path: '/login',
         component: () => import('src/pages/LoginPage.vue'),
       },
@@ -29,9 +19,22 @@ const routes: RouteRecordRaw[] = [
         component: () => import('src/pages/UserPage.vue'),
         props: (route) => ({ id: route.params.id }),
       },
+      {
+        path: '/entry/:id',
+        component: () => import('src/pages/EntryPage.vue'),
+        props: (route) => ({ id: route.params.id }),
+      },
+      {
+        path: '/watchlist/:id',
+        component: () => import('src/pages/WatchlistPage.vue'),
+        props: (route) => ({ id: route.params.id }),
+      },
     ],
   },
-
+  {
+    path: '/unauthorized',
+    component: () => import('pages/ErrorUnauthorized.vue'),
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
