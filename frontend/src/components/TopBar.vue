9d2e74d37e770982723b5edc4ea0eb9d021edbcd
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-05 03:28:27 +0300

    feat(frontend): :sparkles: enchanced topbar

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -11,15 +11,58 @@
       </q-toolbar-title>
     </q-toolbar>
   </q-header>
-  <q-drawer v-model="rightDrawerOpen" side="left" bordered elevated>
-    <q-list dense> </q-list>
+  <q-drawer v-model="rightDrawerOpen" side="left" elevated>
+    <q-list padding>
+      <q-item to="/" exact>
+        <q-item-section avatar>
+          <q-icon color="accent" name="house" />
+        </q-item-section>
+        <q-item-section>Home</q-item-section>
+      </q-item>
+      <q-item v-if="loggedIn" clickable @click="logout()">
+        <q-item-section avatar>
+          <q-icon color="accent" name="logout" />
+        </q-item-section>
+        <q-item-section>Logout</q-item-section>
+      </q-item>
+      <q-item v-if="loggedIn" :to="`/watchlist/${userId}`" exact>
+        <q-item-section avatar>
+          <q-icon color="accent" name="view_list" />
+        </q-item-section>
+        <q-item-section>My watchlist</q-item-section>
+      </q-item>
+      <q-item v-if="loggedIn" :to="`/user/${userId}`" exact>
+        <q-item-section avatar>
+          <q-icon color="accent" name="person_outline" />
+        </q-item-section>
+        <q-item-section>Account settings</q-item-section>
+      </q-item>
+      <q-item to="/login" exact v-if="!loggedIn">
+        <q-item-section avatar>
+          <q-icon color="accent" name="login" />
+        </q-item-section>
+        <q-item-section>Login</q-item-section>
+      </q-item>
+      <q-item to="/register" exact v-if="!loggedIn">
+        <q-item-section avatar>
+          <q-icon color="accent" name="person_add" />
+        </q-item-section>
+        <q-item-section>Register</q-item-section>
+      </q-item>
+    </q-list>
   </q-drawer>
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
 import { ref } from 'vue';
+import { useUserStore } from '../stores/user.store';
+
 const rightDrawerOpen = ref(false);
 function toggleRightDrawer() {
   rightDrawerOpen.value = !rightDrawerOpen.value;
 }
+const store = useUserStore();
+const { loggedIn, userId } = storeToRefs(store);
+const logout = store.logout;
 </script>
