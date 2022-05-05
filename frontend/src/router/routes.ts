cc2c2f86696dc5767cd179d54b99012d65bd5558
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-05 14:16:52 +0300

    feat(frontend): :sparkles: added guard on user settings

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,45 +1,67 @@
 import { RouteRecordRaw } from 'vue-router';
+import { useUserStore } from '../stores/user.store';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('src/pages/LandingPage.vue') },
+      {
+        path: '',
+        component: () => import('src/pages/LandingPage.vue'),
+        name: 'landing',
+      },
       {
         path: '/login',
         component: () => import('src/pages/LoginPage.vue'),
+        name: 'login',
       },
       {
         path: '/register',
         component: () => import('src/pages/RegisterPage.vue'),
+        name: 'register',
       },
       {
         path: '/user/:id',
         component: () => import('src/pages/UserPage.vue'),
         props: (route) => ({ id: route.params.id }),
+        name: 'user',
+        beforeEnter: (to, _from, next) => {
+          const store = useUserStore();
+          if (!store.loggedIn) {
+            next('/login');
+          } else if (to.params.id === store.userId) {
+            next();
+          } else {
+            next('/unauthorized');
+          }
+        },
       },
       {
         path: '/entry/:id',
         component: () => import('src/pages/EntryPage.vue'),
         props: (route) => ({ id: route.params.id }),
+        name: 'entry',
       },
       {
         path: '/watchlist/:id',
         component: () => import('src/pages/WatchlistPage.vue'),
         props: (route) => ({ id: route.params.id }),
+        name: 'watchlist',
       },
     ],
   },
   {
     path: '/unauthorized',
     component: () => import('pages/ErrorUnauthorized.vue'),
+    name: 'unauthorized',
   },
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
+    name: '404',
   },
 ];
 
