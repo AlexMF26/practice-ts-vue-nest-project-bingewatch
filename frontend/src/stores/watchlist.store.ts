62db019bbbecf1621bca9aef2918e1493d7c4392
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-06 16:45:48 +0300

    feat(frontend): :sparkles: added watchlist actions

--- frontend/src/stores/watchlist.store.ts
+++ frontend/src/stores/watchlist.store.ts
@@ -1,5 +1,45 @@
 import { defineStore } from 'pinia';
+import { api } from '../boot/axios';
+import {
+  CreateItemDto,
+  DetailedWatchlistItemEntity,
+  UpdateItemDto,
+  WatchlistItemEntity,
+} from '../types/api/interface';
 
 export const useWatchlistStore = defineStore('watchlist', {
-  actions: {},
+  actions: {
+    async getWatchlist(userId: string) {
+      const response = await api.get<DetailedWatchlistItemEntity[]>(
+        `/watchlist/${userId}`
+      );
+      return response.data;
+    },
+    async getWatchListItem(userId: string, imdbId: string) {
+      const response = await api.get<WatchlistItemEntity>(
+        `/watchlist/${userId}/${imdbId}`
+      );
+      return response.data;
+    },
+    async addWatchListItem(createItemDto: CreateItemDto) {
+      const response = await api.post<WatchlistItemEntity>(
+        '/watchlist/item',
+        createItemDto
+      );
+      return response.data;
+    },
+    async deleteWatchListItem(itemId: string) {
+      const response = await api.delete<WatchlistItemEntity>(
+        `/watchlist/item/${itemId}`
+      );
+      return response.data;
+    },
+    async updateWatchListItem(itemId: string, updateItemDto: UpdateItemDto) {
+      const response = await api.put<WatchlistItemEntity>(
+        `/watchlist/item/${itemId}`,
+        updateItemDto
+      );
+      return response.data;
+    },
+  },
 });
