0686f0f8714d5e67aec45238f336cf551e41f5e9
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-06 19:35:24 +0300

    feat(frontend): :sparkles: add poster component

---
+++ frontend/src/components/PosterComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <img
+    :src="props.posterUrl"
+    alt="poster"
+    width="250"
+    height="300"
+    v-if="props.posterUrl !== 'N/A'"
+  />
+  <img
+    src="~assets/document-missing.svg"
+    alt="poster"
+    width="250"
+    height="300"
+    v-else
+  />
+</template>
+<script setup lang="ts">
+export type Props = {
+  posterUrl: string;
+};
+const props = defineProps<Props>();
+</script>
