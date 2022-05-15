8b309d99f08aeb27dec9bfdbde25441ee9d5bbc7
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-15 19:02:05 +0300

    refactor(frontend): :recycle: refactor stores

--- frontend/src/stores/entries.store.ts
+++ frontend/src/stores/entries.store.ts
@@ -1,8 +1,25 @@
 import { defineStore } from 'pinia';
 import { api } from '../boot/axios';
-import { EntryEntity, EntrySearchResult } from '../types/api/interface';
+import {
+  EntryEntity,
+  EntrySearchResult,
+  WatchlistItemEntity,
+} from '../types/api/interface';
+import { useAuthStore } from './auth.store';
+import { useWatchlistStore } from './watchlist.store';
+
+export type EntryState = {
+  entry: EntryEntity | null;
+  userData: Omit<WatchlistItemEntity, 'entry'> | null;
+};
 
 export const useEntriesStore = defineStore('entries', {
+  state: () =>
+    ({
+      entry: null,
+      userData: null,
+    } as EntryState),
+
   actions: {
     async query(query: string) {
       const response = await api.post<EntrySearchResult[]>(
@@ -17,8 +34,30 @@ export const useEntriesStore = defineStore('entries', {
       return response.data;
     },
     async get(id: string) {
+      await this.getEntry(id);
+      const authStore = useAuthStore();
+      const loggedIn = authStore.loggedIn;
+      if (loggedIn) {
+        const watchlistStore = useWatchlistStore();
+        const userId = authStore.userId;
+        await watchlistStore.getWatchListItem(userId, id);
+      } else {
+        this.userData = null;
+      }
+    },
+    async getEntry(id: string) {
       const response = await api.get<EntryEntity>(`/entries/${id}`);
+      this.entry = response.data;
       return response.data;
+    },
+    async getUserData(imdbId: string, userId: string) {
+      const watchlistStore = useWatchlistStore();
+      const detailedEntry = await watchlistStore.getWatchListItem(
+        userId,
+        imdbId
+      );
+      this.userData = detailedEntry;
+      return this.userData;
     },
   },
 });
--- frontend/src/stores/watchlist.store.ts
+++ frontend/src/stores/watchlist.store.ts
@@ -6,14 +6,25 @@ import {
   UpdateItemDto,
   WatchlistItemEntity,
 } from '../types/api/interface';
+import { useEntriesStore } from './entries.store';
+
+export type WatchlistState = {
+  items: DetailedWatchlistItemEntity[];
+};
 
 export const useWatchlistStore = defineStore('watchlist', {
+  state: () =>
+    ({
+      items: [],
+    } as WatchlistState),
+
   actions: {
     async getWatchlist(userId: string) {
       const response = await api.get<DetailedWatchlistItemEntity[]>(
         `/watchlist/${userId}`
       );
-      return response.data;
+      this.items = response.data;
+      return this.items;
     },
     async getWatchListItem(userId: string, imdbId: string) {
       const response = await api.get<WatchlistItemEntity>(
@@ -26,19 +37,37 @@ export const useWatchlistStore = defineStore('watchlist', {
         '/watchlist/item',
         createItemDto
       );
+      const entryStore = useEntriesStore();
+      const entry = await entryStore.getEntry(createItemDto.imdbId);
+      const { progress, rating, id } = response.data;
+      this.items.push({
+        entry: entry,
+        progress,
+        rating,
+        id,
+      });
       return response.data;
     },
     async deleteWatchListItem(itemId: string) {
       const response = await api.delete<WatchlistItemEntity>(
         `/watchlist/item/${itemId}`
       );
-      return response.data;
+      const { id } = response.data;
+      this.items = this.items.filter((item) => item.id !== id);
+      return null;
     },
     async updateWatchListItem(itemId: string, updateItemDto: UpdateItemDto) {
       const response = await api.put<WatchlistItemEntity>(
         `/watchlist/item/${itemId}`,
         updateItemDto
       );
+      const index = this.items.findIndex((item) => item.id === itemId);
+      if (index !== -1) {
+        const entryStore = useEntriesStore();
+        const entry = await entryStore.getEntry(this.items[index].entry.imdbId);
+        const { progress, rating, id } = response.data;
+        this.items[index] = { entry, progress, rating, id };
+      }
       return response.data;
     },
   },
