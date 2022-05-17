73262b440cca37a062821c3378c9f1c6523f1df9
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-17 22:34:31 +0300

    fix(frontend): :lipstick: align columns

--- frontend/src/components/WatchlistComponent.vue
+++ frontend/src/components/WatchlistComponent.vue
@@ -32,6 +32,7 @@ import WatchlistItemRating from './WatchlistItemRating.vue';
 import { storeToRefs } from 'pinia';
 import { useWatchlistStore } from '../stores/watchlist.store';
 import { DetailedWatchlistItemEntity } from '../types/api/interface';
+import { QTableProps } from 'quasar';
 
 export type Props = {
   isOwner: boolean;
@@ -42,22 +43,25 @@ const componentProps = defineProps<Props>();
 const watchlistStore = useWatchlistStore();
 const { items } = storeToRefs(watchlistStore);
 
-const columns = [
+const columns: QTableProps['columns'] = [
   {
     name: 'title',
     label: 'Title',
     field: (row: DetailedWatchlistItemEntity) => row.entry.title,
+    align: 'center',
   },
 
   {
     name: 'progress',
     label: 'Progress',
     field: (row: DetailedWatchlistItemEntity) => row.progress,
+    align: 'right',
   },
   {
     name: 'rating',
     label: 'Rating',
     field: (row: DetailedWatchlistItemEntity) => row.rating ?? 'N/A',
+    align: 'center',
   },
 ];
 </script>
