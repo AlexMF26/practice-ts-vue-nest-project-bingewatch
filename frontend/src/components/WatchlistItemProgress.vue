ac4a5db41423aa3159db13f8c8b5360af18067cd
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-17 22:19:45 +0300

    feat(frontend): :sparkles: watchlist item progress

--- frontend/src/components/WatchlistItemProgress.vue
+++ frontend/src/components/WatchlistItemProgress.vue
@@ -1,11 +1,30 @@
 <template>
-  {{ item?.progress }}/{{
-              item?.entry.seasons.length !== 0
-                ? item?.entry.seasons.reduce((acc:number, cur:{episodes:number}) => {
-                    return acc + cur.episodes;
-                  }, 0)
-                : 1
-  }}
+  <div class="row float-right items-center">
+    <q-btn
+      dense
+      color="primary"
+      icon="remove"
+      round
+      text-color="secondary"
+      class="on-left"
+      :disabled="(item?.progress ?? 0) <= 0"
+      @click="remove"
+    />
+    <div v-if="type == 'movie'">
+      {{ item?.progress === 1 ? 'Completed' : 'Not completed' }}
+    </div>
+    <div v-else>{{ item?.progress }}/{{ maxProgress }}</div>
+    <q-btn
+      dense
+      color="primary"
+      icon="add"
+      round
+      text-color="secondary"
+      class="on-right"
+      :disabled="(item?.progress ?? 0) >= (maxProgress ?? 1)"
+      @click="add"
+    />
+  </div>
 </template>
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
@@ -22,4 +41,25 @@ const props = defineProps<Props>();
 const watchlistStore = useWatchlistStore();
 const { items } = storeToRefs(watchlistStore);
 const item = computed(() => items.value.find((i) => i.id === props.id));
+const maxProgress = computed(() =>
+  item.value?.entry.seasons.length !== 0
+    ? item.value?.entry.seasons.reduce(
+        (acc: number, cur: { episodes: number }) => {
+          return acc + cur.episodes;
+        },
+        0
+      )
+    : 1
+);
+const type = computed(() =>
+  item.value?.entry.seasons.length !== 0 ? 'series' : 'movie'
+);
+
+async function add() {
+  await watchlistStore.updateWatchListItem(props.id, { progress: 1 });
+}
+
+async function remove() {
+  await watchlistStore.updateWatchListItem(props.id, { progress: -1 });
+}
 </script>
