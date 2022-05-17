17e2b4362a483471c1980fd8621abf91abc5c52a
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-17 13:52:49 +0300

    feat(frontend): :sparkles: added watchlist item title component

---
+++ frontend/src/components/WatchlistItemTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-chip
+    color="primary"
+    text-color="secondary"
+    clickable
+    @click="$router.push('/entry/' + item?.entry.imdbId)"
+  >
+    {{ item?.entry.title }}
+  </q-chip>
+</template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
+import { useWatchlistStore } from '../stores/watchlist.store';
+
+export type Props = {
+  id: string;
+};
+
+const props = defineProps<Props>();
+
+const watchlistStore = useWatchlistStore();
+const { items } = storeToRefs(watchlistStore);
+const item = computed(() => items.value.find((i) => i.id === props.id));
+</script>
