bc817bac23a3b24bf20ac8134e31c939b4f7d753
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-20 09:17:29 +0300

    feat(backend): :sparkles: added opinions listener

---
+++ backend/src/Aplication/Logic/Listeners/opinions.listener.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+import { DeleteOpinionEvent } from '../../../Domain/Events/DeleteOpinion.event';
+import { OpinionsService } from '../Services/opinions.service';
+
+@Injectable()
+export class OpinionsListener {
+  private readonly logger = new Logger(OpinionsListener.name);
+
+  public constructor(private readonly opinionsService: OpinionsService) {}
+
+  @OnEvent(DeleteOpinionEvent.name)
+  public async deleteOpinion(payload: DeleteOpinionEvent) {
+    this.logger.log(`Received event: ${DeleteOpinionEvent.name}`);
+    try {
+      await this.opinionsService.deleteOpinionSafely(payload.id);
+    } catch (error) {
+      this.logger.error(error.message);
+      throw error;
+    }
+  }
+}
