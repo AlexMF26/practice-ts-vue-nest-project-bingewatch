4aa15f31524b8cd21beecfdbb34832f0a02f38a0
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-05-20 09:04:57 +0300

    fix(backend): :card_file_box: db migration

--- backend/src/Infrastructure/Persistence/Repository/Prisma/migrations/20220519180703_add_reviews/migration.sql
+++ backend/src/Infrastructure/Persistence/Repository/Prisma/migrations/20220519180703_add_reviews/migration.sql
@@ -2,22 +2,18 @@
 CREATE TABLE "Opinion" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "authorId" UUID,
+    "entryImdb" TEXT,
     "replyToId" UUID,
-    "watchlistItemId" UUID NOT NULL,
-    "text" TEXT NOT NULL DEFAULT E'',
-    "hidden" BOOLEAN NOT NULL DEFAULT false,
+    "text" TEXT,
 
     CONSTRAINT "Opinion_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "Opinion_watchlistItemId_key" ON "Opinion"("watchlistItemId");
 
 -- AddForeignKey
 ALTER TABLE "Opinion" ADD CONSTRAINT "Opinion_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Opinion" ADD CONSTRAINT "Opinion_watchlistItemId_fkey" FOREIGN KEY ("watchlistItemId") REFERENCES "WatchlistItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Opinion" ADD CONSTRAINT "Opinion_entryImdb_fkey" FOREIGN KEY ("entryImdb") REFERENCES "Entry"("imdbId") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Opinion" ADD CONSTRAINT "Opinion_replyToId_fkey" FOREIGN KEY ("replyToId") REFERENCES "Opinion"("id") ON DELETE SET NULL ON UPDATE CASCADE;
