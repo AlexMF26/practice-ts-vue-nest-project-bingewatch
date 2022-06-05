da4d5058be5cf6e651a6209d7163e4746161868a
Alex-Mihai FIROIU <alexmihaifiroiu26@gmail.com>
2022-06-05 18:43:44 +0300

    fix(frontend): :sparkles: allow i18n to be used outside components

--- frontend/src/boot/i18n.ts
+++ frontend/src/boot/i18n.ts
@@ -1,16 +1,18 @@
 import { boot } from 'quasar/wrappers';
 import { createI18n } from 'vue-i18n';
-
 import messages from 'src/i18n';
 
-export default boot(({ app }) => {
-  const i18n = createI18n({
-    globalInjection: true,
-    locale: 'ro',
-    fallbackLocale: 'en',
-    messages,
-  });
+const i18n = createI18n({
+  globalInjection: true,
+  locale: 'ro',
+  fallbackLocale: 'en',
+  messages,
+});
 
+export default boot(({ app }) => {
   // Set i18n instance on app
   app.use(i18n);
 });
+
+// for use outside of components
+export { i18n };
